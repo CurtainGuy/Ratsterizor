e91811eb33ff172c6ffa700d0d41484bd28b0a10
Wessel Gordijn <wesselsimon@hotmail.com>
2018-05-31 16:42:21 +0200

    SPACING DONE RIGHT

---
+++ Ratsterizor/scenegraph.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+// minimal OpenTK rendering framework for UU/INFOGR
+// Jacco Bikker, 2016
+
+namespace Template_P3 {
+
+    class Scenegraph
+    {
+        public Scenegraph()
+        {
+
+        }
+    }
+} // namespace Template_P3
